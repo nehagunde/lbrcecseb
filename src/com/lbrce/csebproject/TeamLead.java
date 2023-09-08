6dd536c54c15ec8d07517e52479913bc369c0604
AdmiN <AdmiN@ITWS13>
2023-09-08 15:00:26 +0530

    Chandana

--- src/com/lbrce/csebproject/TeamLead.java
+++ src/com/lbrce/csebproject/TeamLead.java
@@ -1,10 +1,14 @@
 package com.lbrce.csebproject;
 
 public class TeamLead {
+	public static void cse69() {
+		System.out.println("This is Chandana");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+	
 
 }
